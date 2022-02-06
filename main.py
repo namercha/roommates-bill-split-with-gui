14da1d067dd2e95a82088c1c7ce45c9fa88e0597
nabil <merchant.nabil@gmail.com>
2022-02-06 11:25:14 -0600

    Laying down skeleton for Flask app

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask.views import MethodView
+from wtforms import Form
+from flask import Flask
+
+app = Flask(__name__)
+
+
+class HomePage(MethodView):
+
+    def get(self):
+        return "Hello"
+
+
+class BillFormPage(MethodView):
+
+    def get(self):
+        return "I am the bill form page!"
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(Form):
+    pass
+
+
+app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
+app.add_url_rule('/bill', view_func=BillFormPage.as_view('bill_form_page'))
+
+app.run()
