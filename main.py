5109b0291e7773b61f070228de2b33d652708244
nabil <merchant.nabil@gmail.com>
2022-02-06 21:01:58 -0600

    Displaying results on the same page as the forms instead of a new page.

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ class BillFormPage(MethodView):
         bill_form = BillForm()
         return render_template('bill_form_page.html', billform=bill_form)
 
-
-class ResultsPage(MethodView):
-
     def post(self):
         billform = BillForm(request.form)
         amount = float(billform.amount.data)
@@ -34,11 +31,36 @@ class ResultsPage(MethodView):
         name2 = billform.name2.data
         days_in_house_2 = float(billform.days_in_house2.data)
         roommate2 = room.Roommate(name2, days_in_house_2)
-        return render_template('results.html',
-                               name1=roommate1.name, 
+        return render_template('bill_form_page.html',
+                               billform=billform,
+                               result=True,
+                               name1=roommate1.name,
                                amount1=roommate1.pays(the_bill, roommate2),
                                name2=roommate2.name,
                                amount2=roommate2.pays(the_bill, roommate1))
+
+
+class ResultsPage(MethodView):
+    pass
+
+    # def post(self):
+    #     billform = BillForm(request.form)
+    #     amount = float(billform.amount.data)
+    #     period = billform.period.data
+    #     the_bill = room.Bill(amount, period)
+    #
+    #     name1 = billform.name1.data
+    #     days_in_house_1 = float(billform.days_in_house1.data)
+    #     roommate1 = room.Roommate(name1, days_in_house_1)
+    #
+    #     name2 = billform.name2.data
+    #     days_in_house_2 = float(billform.days_in_house2.data)
+    #     roommate2 = room.Roommate(name2, days_in_house_2)
+    #     return render_template('results.html',
+    #                            name1=roommate1.name,
+    #                            amount1=roommate1.pays(the_bill, roommate2),
+    #                            name2=roommate2.name,
+    #                            amount2=roommate2.pays(the_bill, roommate1))
 
 
 class BillForm(Form):
@@ -55,7 +77,7 @@ class BillForm(Form):
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
-app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form_page'))
-app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
+app.add_url_rule('/bill_form_page', view_func=BillFormPage.as_view('bill_form_page'))
+# app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
